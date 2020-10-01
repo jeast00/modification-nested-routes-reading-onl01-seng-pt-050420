4f9dd155a1084183763dd8759b483c4b8232e50a
Jonathan East <jonathan_east@ymail.com>
2020-10-01 16:29:50 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,7 @@
 module PostsHelper
-  
+  def author_id_field(post)
+    if post.author.nil?
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    end
+  end
 end
